2de9044d2c81a31d6b0d29fb4f821b85e5800b9d
Carl Lazaro <carljasonlazaro@gmail.com>
2026-08-04 18:09:38 +0800

    Add files via upload

---
+++ sleep.sh
@@ -0,0 +1,12 @@
+echo "Get ready for the take off"
+sleep 3 
+echo "Launching into space" 
+sleep 3 
+echo "In 3" 
+sleep 3 
+echo "2" 
+sleep 3 
+echo "1" 
+sleep 3 
+echo "*The rocket takes off*"
+
